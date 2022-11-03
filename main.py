974efb922cf27ba448bb091076c8c93491dac865
alexrao02 <1520427937@qq.com>
2022-11-04 00:01:18 +0800

    整合mlab方法进入main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
 import cv2 as cv
-from PIL import Image, ImageFilter
+from PIL import Image
 
 
 def euclidean_distance(x1,y1,x2,y2):
@@ -45,10 +45,24 @@ def similarity_func(difference,sigma_r):
     s=np.exp(-0.5*np.square(difference/sigma_r))
     return s
 
+def fai(t):
+    '''
+        XYZ空间 to MLab空间换算函数
+    '''
+    if t <= 0.012546391983472:
+        res = 5.851875995763328 * t + 0.125874125874126
+    else:
+        res = t ** 0.3684
+    return float(res)
+
+
+
+
+
 #用于彩色图的双边滤波器
 def bilateral_filtering_color(img,kernel_size,sigma,space):
     '''
-    双边滤波操作(彩色图), RGB三通道
+    双边滤波操作(彩色图)
     :param init_image: 原始图像路径, type=string
     :param kernel_size: filter大小, type=list
     :param sigma: sigma_d和sigma_r的值, type=list
@@ -65,14 +79,39 @@ def bilateral_filtering_color(img,kernel_size,sigma,space):
         init_image = cv.cvtColor(img_in, cv.COLOR_BGR2HLS)
     if space == 'Luv':
         init_image = cv.cvtColor(img_in, cv.COLOR_BGR2Luv)
+    if space == 'MLAB':
+        init_image = cv.cvtColor(img_in, cv.COLOR_BGR2XYZ)
+    if space == 'RGB':
+        filtered_image_OpenCV = cv.bilateralFilter(img_in, 3, 10.0, 10.0)
+        filtered_image_cv = Image.fromarray(cv.cvtColor(filtered_image_OpenCV, cv.COLOR_BGR2RGB))
+        filtered_image_cv.save('RGB.png')
+        return 0
+
     init_image_mat = np.array(init_image)  # 将原始图像转化成numpy数组
+    filtered_image_mat = np.zeros_like(init_image_mat)
+
+    # 将xyz空间转换为mlab空间
+    if space == 'MLAB':
+        for channel in range(init_image_mat.shape[2]):
+            for x in range(init_image_mat.shape[0]):
+                for y in range(init_image_mat.shape[1]):
+                    mlab_x = fai(init_image_mat[x][y][0] / 95.047)
+                    mlab_y = fai(init_image_mat[x][y][1] / 100)
+                    mlab_z = fai(init_image_mat[x][y][2] / 108.883)
+                    if channel == 0:
+                        filtered_image_mat[x][y][channel] = 114.4 * mlab_y - 14.4
+                    if channel == 1:
+                        filtered_image_mat[x][y][channel] = 311.5 * (mlab_x - mlab_y)
+                    if channel == 2:
+                        filtered_image_mat[x][y][channel] = 111 * (mlab_y - mlab_z)
+
     sigma_d=sigma[0]
     sigma_r=sigma[1]
-    filtered_image_mat=np.zeros_like(init_image_mat)
-    #对图像的每一个像素值进行双边滤波操作, 一共有RGB三个通道分别操作
-    for channel in range(init_image_mat.shape[2]):
-        for x in range(init_image_mat.shape[0]):
-            for y in range(init_image_mat.shape[1]):
+    filtered_image_mat_new=np.zeros_like(filtered_image_mat)
+    #对图像的每一个像素值进行双边滤波操作, 三个通道分别操作
+    for channel in range(filtered_image_mat.shape[2]):
+        for x in range(filtered_image_mat.shape[0]):
+            for y in range(filtered_image_mat.shape[1]):
                 print("finish x=%d,y=%d"%(x,y))
                 #每个像素的操作范围为以其为中心的kernel
                 filtered_pixel=0
@@ -83,27 +122,33 @@ def bilateral_filtering_color(img,kernel_size,sigma,space):
                         now_x=x+i
                         now_y=y+j
                         # 特判超出边界的不合法坐标
-                        if now_x<0 or now_x>=init_image_mat.shape[0] or now_y<0 or now_y>=init_image_mat.shape[1]:
+                        if now_x<0 or now_x>=filtered_image_mat.shape[0] or now_y<0 or now_y>=filtered_image_mat.shape[1]:
                             continue
                         dis=euclidean_distance(x,y,now_x,now_y) #距离
-                        diff=pixel_difference(init_image_mat[x][y][channel],init_image_mat[now_x][now_y][channel]) #像素差
+                        diff=pixel_difference(filtered_image_mat[x][y][channel],filtered_image_mat[now_x][now_y][channel]) #像素差
                         closeness=closeness_func(dis,sigma_d) #双边滤波函数的第一项权重值
                         similarity=similarity_func(diff,sigma_r) #双边滤波函数的第二项权重值
                         weight=closeness*similarity #双边滤波函数的权重: 第一项*第二项
                         filtered_pixel+=weight*init_image_mat[now_x][now_y][channel] #按权重聚合周边像素点值
                         weight_sum+=weight #记录总权重,用于标准化
                 #标准化及更新
-                filtered_image_mat[x][y][channel]=int(round(filtered_pixel/weight_sum))
+                filtered_image_mat_new[x][y][channel]=int(round(filtered_pixel/weight_sum))
+
+    filtered_image=Image.fromarray(filtered_image_mat_new)  #格式转换
+
+    # 颜色空间转换
     if space == 'Lab':
-        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat, cv.COLOR_Lab2RGB))
+        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat_new, cv.COLOR_Lab2RGB))
     if space == 'XYZ':
-        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat, cv.COLOR_XYZ2RGB))
+        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat_new, cv.COLOR_XYZ2RGB))
     if space == 'HSV':
-        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat, cv.COLOR_HSV2RGB))
+        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat_new, cv.COLOR_HSV2RGB))
     if space == 'HLS':
-        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat, cv.COLOR_HLS2RGB))
+        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat_new, cv.COLOR_HLS2RGB))
     if space == 'Luv':
-        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat, cv.COLOR_Luv2RGB))
+        filtered_image = Image.fromarray(cv.cvtColor(filtered_image_mat_new, cv.COLOR_Luv2RGB))
+    if space == 'MLAB':
+        filtered_image=Image.fromarray(filtered_image_mat_new)
     filtered_image.save(space + '.png')
     return filtered_image
 
@@ -112,22 +157,11 @@ if __name__=='__main__':
     img = cv.imread("red.png",1)
     kernel_size=[3,3]
     sigma=[10,10]
+    filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'RGB')
     filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'Lab')
     filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'XYZ')
     filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'HLS')
     filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'Luv')
-    # filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'HSV')
+    filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'MLAB')
+    filtered_image = bilateral_filtering_color(img, kernel_size, sigma, 'HSV')
 
-    # filtered_image_OpenCV = cv.bilateralFilter(img, 3, 10.0, 10.0)
-    #
-    # im1 = Image.open(r"red.png")
-    # applying the Gaussian Blur filter
-    # im2 = im1.filter(ImageFilter.GaussianBlur(radius=5))
-
-
-    # img_lenna = cv.imread('lena.png')
-    # init_image = cv.cvtColor(img_lenna, cv.COLOR_BGR2Lab)
-    # cv.imwrite("1.png",init_image)
-    # img = cv.imread("red.png", 1)
-    # filtered_image_OpenCV = cv.bilateralFilter(img, 3, 10.0, 10.0)
-    # cv.imwrite("new.png", filtered_image_OpenCV)
